c295879c16c3d3755f92e6d7c8d6f73ac1134b88
KalinVasilevSV <kalinsv@abv.bg>
2019-05-28 13:18:46 +0300

    initital work on morning of 28.05

---
+++ 01.Sort-Times/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01.Sort_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> times = new SortedSet<string>(Console.ReadLine().Trim().Split().ToList());
+            
+            Console.WriteLine(String.Join(", ", times));
+        }
+    }
+}
---
+++ 02.Odd-Filter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02.Odd_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(Console.ReadLine().Trim().Split().Select(int.Parse).ToList());
+
+            for (int i = 0; i < numbers.Count; i++) {
+                if (numbers[i] % 2 == 1) {
+                    numbers.RemoveAt(i);
+                    i -= 1;
+                }
+                
+            }
+
+            for (int i = 0; i < numbers.Count; i++) {
+                if (numbers[i] > Math.Ceiling(numbers.Average())) numbers[i]++;
+                else if (numbers[i] <= Math.Ceiling(numbers.Average())) numbers[i]--;
+            }
+
+            Console.WriteLine(String.Join(" ",numbers));
+        }
+    }
+}
