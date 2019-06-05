8fb1c9faf59d1db44b0b784992b74b3d32d9f79e
KalinVasilevSV <kalinsv@abv.bg>
2019-06-05 12:02:05 +0300

    final work on this repo on 05.06 morning

--- 07.Take-Skip-Rope/Program.cs
+++ 07.Take-Skip-Rope/Program.cs
@@ -4,11 +4,41 @@ using System.Collections.Generic;
 
 namespace _07.Take_Skip_Rope
 {
+    //Finished
     class Program
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            List<int> digits = new List<int>();
+            List<string> symbols = new List<string>();
+
+            for (int i = 0; i < input.Length; i++) {
+                try {
+                    digits.Add(int.Parse(input[i].ToString()));
+                }
+                catch (FormatException) {
+                    symbols.Add(input[i].ToString());
+                }
+            }
+            string symbolString = String.Join("",symbols);
+
+            List<int> takeList = new List<int>();
+            List<int> skipList = new List<int>();
+            for (int i = 0; i < digits.Count; i++) {
+                if (i % 2 == 0) takeList.Add(digits[i]);
+                else skipList.Add(digits[i]);
+            }
+
+            string result = string.Empty;
+            int startIndex = 0;
+            for (int i = 0; i <takeList.Count;i++) {
+                result += symbolString.Substring(startIndex,(startIndex+takeList[i]>symbolString.Length?(symbolString.Length-startIndex):takeList[i]));
+                startIndex += (skipList[i]+takeList[i]);
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
